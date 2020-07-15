dd910d7e1517b57b8398bb794c58d136b3b5ab9e
Uthpala Herath <ukh0001@mix.wvu.edu>
2020-07-15 18:28:28 -0400

    Added netcdf4 to setup.py requirements

--- setup.py
+++ setup.py
@@ -23,14 +23,14 @@ ext_modules = cythonize([Extension("scripts.append_df", ["scripts/append_df.pyx"
 setup(
     name="convertSPEI",
     description="A Python script to convert Standardised Precipitation-Evapotranspiration Index (SPEI) data from the netcdf format to csv and Excel formats.",
-    version="0.5",
+    version="0.6",
     author="Uthpala Herath",
     author_email="ukh0001@mix.wvu.edu",
     url="https://github.com/uthpalaherath/convertSPEI",
-    download_url="https://github.com/uthpalaherath/convertSPEI/archive/0.5.tar.gz",
+    download_url="https://github.com/uthpalaherath/convertSPEI/archive/0.6.tar.gz",
     license="LICENSE.txt",
     scripts=["scripts/convertSPEI.py", "scripts/append_df.pyx", "scripts/append_df.c"],
-    install_requires=["pandas", "cython"],
+    install_requires=["pandas", "cython", "netcdf4"],
     keywords=["netcdf", "spei", "atmosphereicsciences", "earthsciences"],
     ext_modules=ext_modules,
 )
