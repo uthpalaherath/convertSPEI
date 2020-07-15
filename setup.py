d77c4230309a0910db1f468fc678de976dbe8df1
Uthpala Herath <ukh0001@mix.wvu.edu>
2020-07-15 18:01:40 -0400

    Updated manifest

--- setup.py
+++ setup.py
@@ -23,11 +23,11 @@ ext_modules = cythonize([Extension("scripts.append_df", ["scripts/append_df.pyx"
 setup(
     name="convertSPEI",
     description="A Python script to convert Standardised Precipitation-Evapotranspiration Index (SPEI) data from the netcdf format to csv and Excel formats.",
-    version="0.1",
+    version="0.3",
     author="Uthpala Herath",
     author_email="ukh0001@mix.wvu.edu",
     url="https://github.com/uthpalaherath/convertSPEI",
-    download_url="https://github.com/uthpalaherath/convertSPEI/archive/0.1.tar.gz",
+    download_url="https://github.com/uthpalaherath/convertSPEI/archive/0.3.tar.gz",
     license="LICENSE.txt",
     scripts=["scripts/convertSPEI.py"],
     install_requires=["pandas", "cython"],
