04a42c8a8a95b13ab317e271a0320e01b5927f06
Uthpala Herath <ukh0001@mix.wvu.edu>
2020-07-16 02:31:54 -0400

    Updated setup requirements

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from numpy.distutils.core import Extension, setup
 setup(
     name="convertSPEI",
     description="A Python script to convert Standardised Precipitation-Evapotranspiration Index (SPEI) data from the netcdf format to a csv format.",
-    version="0.7",
+    version="0.8",
     author="Uthpala Herath",
     author_email="ukh0001@mix.wvu.edu",
     url="https://github.com/uthpalaherath/convertSPEI",
-    download_url="https://github.com/uthpalaherath/convertSPEI/archive/0.7.tar.gz",
+    download_url="https://github.com/uthpalaherath/convertSPEI/archive/0.8.tar.gz",
     license="LICENSE.txt",
     scripts=["scripts/convertSPEI.py"],
-    install_requires=["netcdf4", "csv"],
+    install_requires=["netcdf4"],
     keywords=["netcdf", "spei", "atmosphereicsciences", "earthsciences"],
 )
